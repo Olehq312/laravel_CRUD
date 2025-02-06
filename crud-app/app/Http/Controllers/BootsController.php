43203d32fb3ee973450eba42f85f5b7d33120c62
Oleh <118124646+Olehq312@users.noreply.github.com>
2025-02-06 09:59:20 +0100

    i did it

---
+++ crud-app/app/Http/Controllers/BootsController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Boots;
+use Illuminate\Http\Request;
+
+class BootsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Boots $boots)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Boots $boots)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Boots $boots)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Boots $boots)
+    {
+        //
+    }
+}
--- crud-app/routes/web.php
+++ crud-app/routes/web.php
@@ -1,7 +1,9 @@
 <?php
-
+use App\Http\Controllers\BootsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('boots', BootsController::class);
